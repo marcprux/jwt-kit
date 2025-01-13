f75fd6ea0ed663e8a9c8633103d0935917202e9e
Marc Prud'hommeaux <marc@skip.tools>
2025-01-13 17:08:58 -0500

    Update swift-certificates dependency

--- Package.swift
+++ Package.swift
@@ -14,8 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-crypto.git", "3.8.0"..<"5.0.0"),
-        .package(url: "https://github.com/apple/swift-certificates.git", branch: "main"),
-        //.package(url: "https://github.com/apple/swift-certificates.git", from: "1.2.0"),
+        .package(url: "https://github.com/apple/swift-certificates.git", from: "1.2.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
     targets: [
